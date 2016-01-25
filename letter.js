019513dd17b20d88b22b663e270dee352e6d64d2
Mohammed Rizvi <morizvi07@gmail.com>
2016-01-25 18:56:03 -0500

    Created folders, added constructor function for letter.js and put ds store and node module in gitignore folder

---
+++ letter.js
@@ -0,0 +1,14 @@
+function Letter(let) {
+  this.charac = let;
+  this.appear = false;
+
+  this.letterRender = function() {
+    if (this.appear === false) {
+      return "__";
+    } else (this.appear === true) {
+      return this.charac;
+    }
+  }
+}
+
+module.exports = Letter;
