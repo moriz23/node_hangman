9098f92a6da0346c9beccfa7684531897de05458
Mohammed Rizvi <morizvi07@gmail.com>
2016-01-25 20:02:59 -0500

    created the checkletterfound function this checks the array to see if the letter that the user gues is found.

--- word.js
+++ word.js
@@ -1,1 +1,22 @@
 var Letter = require("./letter.js");
+
+function Word(wrd) {
+  this.word = wrd;
+  this.lets = [];
+  this.found = false;
+  this.getLets = function() {
+    for (var i = 0; i < this.word.length; i++) {
+      this.lets.push(new Letter(this.word[i]));
+    }
+  }
+  this.checkIfLetterFound = function(guessLetter) {
+    var whatToReturn = 0;
+    for(var i = 0; i < this.lets.length; i++)
+      if(this.lets[i].charac === guessLetter) {
+        this.lets[i].appear = true;
+        whatToReturn += 1;
+    } 
+  }
+  return whatToReturn;
+};
+
